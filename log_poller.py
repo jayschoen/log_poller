e6fd2d7c292deff691e284bb1ba26f97422f27b1
JaySchoen <jschoen@numbersusa.com>
2017-10-04 12:18:06 -0400

    switch to using pyinotify

--- log_poller.py
+++ log_poller.py
@@ -1,14 +1,40 @@
-import time
-import subprocess
-import select
+import asyncore
+import pyinotify
 
-FILENAME = "test_log.log"
+watched_handle = "/home/jschoen/log_poller/test_log.log"
+watched_contents = ''
+with open(watched_handle) as f:
+  watched_contents = f.readlines()
 
- f = subprocess.Popen(['tail', '-F', FILENAME], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
- p = select.poll() ### select.poll is not supported in BSD???
- p.register(f.stdout) .register is part of .poll...
 
- while True:
-    if p.poll(1):
-        print f.stdout.readline()
-    time.sleep(1)
+def diff_files(previous_contents, handle):
+  
+  modified_contents = ''
+  with open(watched_handle, 'r') as f:
+    modified_contents = f.readlines()
+
+  print list(set(modified_contents) - set(previous_contents))
+
+####
+
+wm = pyinotify.WatchManager()  # Watch Manager
+mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY  # watched events
+
+class EventHandler(pyinotify.ProcessEvent):
+    def process_IN_CREATE(self, event):
+        print "Creating:", event.pathname
+
+    def process_IN_DELETE(self, event):
+        print "Removing:", event.pathname
+
+    def process_IN_MODIFY(self, event):
+        #print event
+        global watched_handle, watched_contents
+        diff_files(watched_contents, watched_handle)
+        with open(watched_handle, 'r') as f:
+          watched_contents = f.readlines() 
+
+notifier = pyinotify.AsyncNotifier(wm, EventHandler())
+wdd = wm.add_watch('/home/jschoen/log_poller', mask, rec=True)
+
+asyncore.loop()
